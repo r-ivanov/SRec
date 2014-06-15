3e08028992133923aa1d7847501839c7df75f351
David Pastor Herranz <d.pastor@alumnos.urjc.es>
2014-06-15 11:50:46 +0200

    Corrección de formato y comentarios de la clase SRec

--- SRec.java
+++ SRec.java
@@ -1,32 +1,29 @@
-/**
-	Esta clase representa la aplicación, invoca a una instancia de la ventana de la aplicación.
-
-	@author Luis Fernández
-	@version Desconocida
-
-*/
-
 import ventanas.*;
 import conf.*;
 import utilidades.*;
 
-public class SRec
-{
+/**
+ * Representa la aplicación.
+ * 
+ * @author Luis Fernández
+ */
+public class SRec {
+	
 	/**
-		Invoca una ventana del visualizador.
-	*/
-	public SRec()
-	{
+	 * Invoca una nueva instancia de la ventana de la aplicación.
+	 */
+	public SRec() {
 		new Ventana();
 	}
 
 	/**
-		Método main de la clase Visualizador. Permite la invocación de la aplicación.
-	*/
-	public static void main(String args[])
-	{
-		if (args.length!=0)
-			System.out.println("\n\n"+Texto.get("SREC_NOARG",Conf.idioma)+"\n\n");
+	 * Método main de la aplicación. Permite la invocación de la ventana de la
+	 * aplicación.
+	 */
+	public static void main(String args[]) {
+		if (args.length != 0) {
+			System.out.println("\n\n" + Texto.get("SREC_NOARG", Conf.idioma) + "\n\n");
+		}
 		new SRec();
-    }
+	}
 }
