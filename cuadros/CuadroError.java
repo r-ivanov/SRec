d84ca2e44ff394971a51953fee70684664686447
David Pastor Herranz <d.pastor@alumnos.urjc.es>
2014-06-19 14:09:50 +0200

    Corrección de formato para la clase CuadroError

--- cuadros/CuadroError.java
+++ cuadros/CuadroError.java
@@ -1,22 +1,6 @@
-/**
-	Representa el cuadro de error
-	
-	@author Antonio Pérez Carrasco
-	@version 2006-2007
-*/
-
 package cuadros;
 
-
-
-
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Window;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -24,332 +8,295 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import java.io.IOException;
-
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 import conf.*;
 import botones.*;
 import utilidades.*;
-import ventanas.*;
 
-public class CuadroError extends Thread implements ActionListener, KeyListener, MouseListener
-{
-	static final long serialVersionUID=01;
+/**
+ * Representa el cuadro de error.
+ * 
+ * @author Antonio Pérez Carrasco
+ * @version 2006-2007
+ */
+public class CuadroError extends Thread implements ActionListener, KeyListener,
+		MouseListener {
 
-	private static int ancho = 550;
-	private static int alto  = 100;
-	JLabel etiqueta, imagen;
-	BotonAceptar aceptar;
+	private static final int ANCHO_CUADRO_POR_DEFECTO = 550;
+	private static final int ALTO_CUADRO_POR_DEFECTO = 100;
 
-	JPanel panel, panelBoton, panelImagen, panelDerecha, panelEtiqueta;
+	private int anchoCuadro;
+	private int altoCuadro;
 
-	String titulo;
-	String etiq;
-	String comando;
-	String boton2;
+	private JLabel etiqueta, imagen;
+	private BotonAceptar aceptar;
 
-	BorderLayout bl, bl0, bl1, bl2;
-	
-	JDialog d;
+	private JPanel panel, panelBoton, panelImagen, panelDerecha, panelEtiqueta;
+
+	private String titulo;
+	private String etiq;
+
+	private BorderLayout bl, bl0, bl1;
+
+	private JDialog d;
 
 	/**
-		Constructor: crea una nueva instancia del cuadro de error
-		
-		@param dialogo dialogo al que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-	*/
-	public CuadroError(JDialog dialogo, String titulo, String etiq)
-	{
-		this(dialogo,titulo,etiq,ancho,alto);
-	}
-	
-	/**
-		Constructor: crea una nueva instancia del cuadro de error
-		
-		@param ventana ventana a la que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-	*/
-	public CuadroError(JFrame ventana, String titulo, String etiq)
-	{
-		this(ventana,titulo,etiq,ancho,alto);
+	 * Constructor: crea una nueva instancia del cuadro de error
+	 * 
+	 * @param dialogo
+	 *            dialogo al que quedará asociado este cuadro
+	 * @param titulo
+	 *            título que llevará el cuadro de error
+	 * @param etiq
+	 *            mensaje que mostrará el cuadro de error
+	 */
+	public CuadroError(JDialog dialogo, String titulo, String etiq) {
+		this(dialogo, titulo, etiq, ANCHO_CUADRO_POR_DEFECTO,
+				ALTO_CUADRO_POR_DEFECTO);
 	}
 
 	/**
-		Constructor: crea una nueva instancia del cuadro de error
-		
-		@param dialogo dialogo al que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param anc ancho del cuadro
-		@param alt alto del cuadro
-	*/
-	public CuadroError(JDialog dialogo,String titulo, String etiq, int anc, int alt)
-	{
-		this(dialogo,titulo,etiq,"","",anc,alt);
+	 * Constructor: crea una nueva instancia del cuadro de error
+	 * 
+	 * @param ventana
+	 *            ventana a la que quedará asociado este cuadro
+	 * @param titulo
+	 *            título que llevará el cuadro de error
+	 * @param etiq
+	 *            mensaje que mostrará el cuadro de error
+	 */
+	public CuadroError(JFrame ventana, String titulo, String etiq) {
+		this(ventana, titulo, etiq, ANCHO_CUADRO_POR_DEFECTO,
+				ALTO_CUADRO_POR_DEFECTO);
 	}
 
 	/**
-		Constructor: crea una nueva instancia del cuadro de error
-		
-		@param ventana ventana a la que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param anc ancho del cuadro
-		@param alt alto del cuadro
-	*/	
-	public CuadroError(JFrame ventana,String titulo, String etiq, int anc, int alt)
-	{
-		this(ventana,titulo,etiq,"","",anc,alt);
+	 * Constructor: crea una nueva instancia del cuadro de error
+	 * 
+	 * @param dialogo
+	 *            dialogo al que quedará asociado este cuadro
+	 * @param titulo
+	 *            título que llevará el cuadro de error
+	 * @param etiq
+	 *            mensaje que mostrará el cuadro de error
+	 * @param anc
+	 *            ancho del cuadro
+	 * @param alt
+	 *            alto del cuadro
+	 */
+	public CuadroError(JDialog dialogo, String titulo, String etiq, int anc,
+			int alt) {
+		this.d = new JDialog(dialogo, true);
+		this.titulo = titulo;
+		this.etiq = etiq;
+		this.anchoCuadro = anc;
+		this.altoCuadro = alt;
+		this.start();
+
 	}
 
 	/**
-		Constructor: crea una nueva instancia del cuadro de error. DESECHADO
-		
-		@param dialogo dialogo al que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param comando comando que ejecutará el cuadro
-		@param boton2 texto que aparecerá en el segundo botón de acción
-	*/	
-	public CuadroError(JDialog dialogo, String titulo, String etiq, String comando, String boton2)
-	{
-		this(dialogo,titulo,etiq,comando,boton2,ancho,alto);
-	}
-	
-	/**
-		Constructor: crea una nueva instancia del cuadro de error. DESECHADO
-		
-		@param ventana ventana a la que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param comando comando que ejecutará el cuadro
-		@param boton2 texto que aparecerá en el segundo botón de acción
-	*/	
-	public CuadroError(JFrame ventana, String titulo, String etiq, String comando, String boton2)
-	{
-		this(ventana,titulo,etiq,comando,boton2,ancho,alto);
-	}
-
-	/**
-		Constructor: crea una nueva instancia del cuadro de error. DESECHADO
-		
-		@param ventana ventana a la que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param comando comando que ejecutará el cuadro
-		@param boton2 texto que aparecerá en el segundo botón de acción
-		@param anc ancho del cuadro
-		@param alt alto del cuadro
-	*/	
-	public CuadroError(JFrame ventana,String titulo, String etiq, String comando, String boton2, int anc, int alt)
-	{
-		d=new JDialog(ventana,true);
-		this.titulo=titulo;
-		this.etiq=etiq;
-		this.comando=comando;
-		this.boton2=boton2;
-		this.ancho=anc;
-		this.alto=alt;
+	 * Constructor: crea una nueva instancia del cuadro de error
+	 * 
+	 * @param ventana
+	 *            ventana a la que quedará asociado este cuadro
+	 * @param titulo
+	 *            título que llevará el cuadro de error
+	 * @param etiq
+	 *            mensaje que mostrará el cuadro de error
+	 * @param anc
+	 *            ancho del cuadro
+	 * @param alt
+	 *            alto del cuadro
+	 */
+	public CuadroError(JFrame ventana, String titulo, String etiq, int anc,
+			int alt) {
+		this.d = new JDialog(ventana, true);
+		this.titulo = titulo;
+		this.etiq = etiq;
+		this.anchoCuadro = anc;
+		this.altoCuadro = alt;
 		this.start();
 	}
 
 	/**
-		Constructor: crea una nueva instancia del cuadro de error. DESECHADO
-		
-		@param dialogo dialogo al que quedará asociado este cuadro
-		@param titulo título que llevará el cuadro de error
-		@param etiq mensaje que mostrará el cuadro de error
-		@param comando comando que ejecutará el cuadro
-		@param boton2 texto que aparecerá en el segundo botón de acción
-		@param anc ancho del cuadro
-		@param alt alto del cuadro
-	*/		
-	public CuadroError(JDialog dialogo,String titulo, String etiq, String comando, String boton2, int anc, int alt)
-	{
-		d=new JDialog(dialogo,true);
-		this.titulo=titulo;
-		this.etiq=etiq;
-		this.comando=comando;
-		this.boton2=boton2;
-		this.ancho=anc;
-		this.alto=alt;
-		this.start();
-	}
-	
-	/**
-		crea una nueva instancia del cuadro de error. DESECHADO
-	*/
-	public void run()
-	{
-		this.comando=comando;
+	 * Ejecuta el thread.
+	 */
+	@Override
+	public void run() {
 
 		// Etiqueta para icono
-		panelImagen = new JPanel();
-		imagen=new JLabel(new ImageIcon("imagenes/error.gif"));
-		imagen.addKeyListener(this);
-		imagen.setHorizontalAlignment(0);
-		imagen.setVerticalAlignment(0);
-		panelImagen.add(imagen);
+		this.panelImagen = new JPanel();
+		this.imagen = new JLabel(new ImageIcon("imagenes/error.gif"));
+		this.imagen.addKeyListener(this);
+		this.imagen.setHorizontalAlignment(0);
+		this.imagen.setVerticalAlignment(0);
+		this.panelImagen.add(this.imagen);
 
-				
 		// Etiqueta de texto
-		etiqueta=new JLabel(this.etiq);
-		etiqueta.addKeyListener(this);
-		etiqueta.setHorizontalAlignment(0);
-		etiqueta.setVerticalAlignment(0);
-		
+		this.etiqueta = new JLabel(this.etiq);
+		this.etiqueta.addKeyListener(this);
+		this.etiqueta.setHorizontalAlignment(0);
+		this.etiqueta.setVerticalAlignment(0);
+
 		// Panel para etiqueta
-		bl2= new BorderLayout();
-		panelEtiqueta = new JPanel();
-		panelEtiqueta.setLayout(bl2);
-		panelEtiqueta.add(etiqueta,BorderLayout.CENTER);
-		
+		this.bl1 = new BorderLayout();
+		this.panelEtiqueta = new JPanel();
+		this.panelEtiqueta.setLayout(this.bl1);
+		this.panelEtiqueta.add(this.etiqueta, BorderLayout.CENTER);
+
 		// Botón Aceptar
-		aceptar=new BotonAceptar();
-		aceptar.addKeyListener(this);
-		aceptar.addMouseListener(this);
-		
+		this.aceptar = new BotonAceptar();
+		this.aceptar.addKeyListener(this);
+		this.aceptar.addMouseListener(this);
+
 		// Panel de botón Aceptar
-		panelBoton = new JPanel();
-		panelBoton.add(aceptar);
-		panelBoton.addKeyListener(this);
-		panelBoton.transferFocus();
+		this.panelBoton = new JPanel();
+		this.panelBoton.add(this.aceptar);
+		this.panelBoton.addKeyListener(this);
+		this.panelBoton.transferFocus();
 
 		// Panel de la derecha
-		bl0= new BorderLayout();
-		panelDerecha = new JPanel();
-		panelDerecha.setLayout(bl0);
-		panelDerecha.add(panelEtiqueta,BorderLayout.CENTER);
-		panelDerecha.add(panelBoton,BorderLayout.SOUTH);
-		panelDerecha.addKeyListener(this);
+		this.bl0 = new BorderLayout();
+		this.panelDerecha = new JPanel();
+		this.panelDerecha.setLayout(this.bl0);
+		this.panelDerecha.add(this.panelEtiqueta, BorderLayout.CENTER);
+		this.panelDerecha.add(this.panelBoton, BorderLayout.SOUTH);
+		this.panelDerecha.addKeyListener(this);
 
 		// Panel general de CuadroError
-		bl= new BorderLayout();
-		panel = new JPanel();
-		panel.setLayout(bl);
-		panel.addKeyListener(this);
+		this.bl = new BorderLayout();
+		this.panel = new JPanel();
+		this.panel.setLayout(this.bl);
+		this.panel.addKeyListener(this);
 
-		panel.add(imagen,BorderLayout.WEST);
-		panel.add(panelDerecha,BorderLayout.CENTER);
-		
-		if (Conf.fichero_log) 	Logger.log_write("ERROR ["+this.titulo+"]: "+this.etiq);
-		
-		d.getContentPane().add(panel);
-		int coord[]=Conf.ubicarCentro(ancho,alto);
-		d.setLocation(coord[0],coord[1]);
-		d.setTitle(titulo);
-		d.setSize(ancho,alto);
-		d.setResizable(false);	
-		d.setVisible(true);
-		aceptar.transferFocus();
-	}
+		this.panel.add(this.imagen, BorderLayout.WEST);
+		this.panel.add(this.panelDerecha, BorderLayout.CENTER);
 
-	/**
-		Gestiona los eventos de acción
-		
-		@param e evento de acción
-	*/
-	public void actionPerformed(ActionEvent e)
-	{
-		d.setVisible(false);
-	}
-	
-	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/			
-	public void keyPressed(KeyEvent e)
-	{
-		int code=e.getKeyCode();
-		//System.out.println("keyPressed");
-	}
-	
-	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/	
-	public void keyReleased(KeyEvent e)
-	{
-		int code=e.getKeyCode();
-		//System.out.println("keyReleased");
-		if (code==KeyEvent.VK_ESCAPE || code==KeyEvent.VK_ENTER)
-			d.setVisible(false);
+		if (Conf.fichero_log) {
+			Logger.log_write("ERROR [" + this.titulo + "]: " + this.etiq);
+		}
+
+		this.d.getContentPane().add(this.panel);
+		int coord[] = Conf.ubicarCentro(this.anchoCuadro, this.altoCuadro);
+		this.d.setLocation(coord[0], coord[1]);
+		this.d.setTitle(this.titulo);
+		this.d.setSize(this.anchoCuadro, this.altoCuadro);
+		this.d.setResizable(false);
+		this.d.setVisible(true);
+		this.aceptar.transferFocus();
 	}
 
 	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/	
-	public void keyTyped(KeyEvent e)
-	{
-		int code=e.getKeyCode();
-		//System.out.println("keyTyped");
-		if (code==KeyEvent.VK_ENTER)
-			d.setVisible(false);
+	 * Gestiona los eventos de acción
+	 * 
+	 * @param e
+	 *            evento de acción
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.d.setVisible(false);
 	}
 
 	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/		
-	public void mouseClicked(MouseEvent e) 
-	{
+	 * Gestiona los eventos de teclado
+	 * 
+	 * @param e
+	 *            evento de teclado
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+
 	}
-	
 
 	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/		
-	public void mouseEntered(MouseEvent e) 
-	{
+	 * Gestiona los eventos de teclado
+	 * 
+	 * @param e
+	 *            evento de teclado
+	 */
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_ESCAPE || code == KeyEvent.VK_ENTER) {
+			this.d.setVisible(false);
+		}
 	}
-	
 
 	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/		
-	public void mouseExited(MouseEvent e) 
-	{
+	 * Gestiona los eventos de teclado
+	 * 
+	 * @param e
+	 *            evento de teclado
+	 */
+	@Override
+	public void keyTyped(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_ENTER) {
+			this.d.setVisible(false);
+		}
 	}
-	
 
 	/**
-		Gestiona los eventos de ratón
+	 * Gestiona los eventos de ratón
+	 * 
+	 * @param e
+	 *            evento de ratón
+	 */
+	@Override
+	public void mouseClicked(MouseEvent e) {
 		
-		@param e evento de ratón
-	*/		
-	public void mousePressed(MouseEvent e) 
-	{
-		if (e.getComponent().equals(aceptar))
-			d.setVisible(false);
 	}
-	
 
 	/**
-		Gestiona los eventos de ratón
+	 * Gestiona los eventos de ratón
+	 * 
+	 * @param e
+	 *            evento de ratón
+	 */
+	@Override
+	public void mouseEntered(MouseEvent e) {
 		
-		@param e evento de ratón
-	*/		
-	public void mouseReleased(MouseEvent e)
-	{
 	}
-	
+
+	/**
+	 * Gestiona los eventos de ratón
+	 * 
+	 * @param e
+	 *            evento de ratón
+	 */
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	/**
+	 * Gestiona los eventos de ratón
+	 * 
+	 * @param e
+	 *            evento de ratón
+	 */
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getComponent().equals(this.aceptar)) {
+			this.d.setVisible(false);
+		}
+	}
+
+	/**
+	 * Gestiona los eventos de ratón
+	 * 
+	 * @param e
+	 *            evento de ratón
+	 */
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
 
 }
