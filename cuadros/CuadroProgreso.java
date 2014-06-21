7fbf64f00a602cc5ebe780f8a20514bb0efe1d6a
David Pastor Herranz <d.pastor@alumnos.urjc.es>
2014-06-21 15:02:29 +0200

    Corrección de formato para la clase CuadroProgreso.

--- cuadros/CuadroProgreso.java
+++ cuadros/CuadroProgreso.java
@@ -1,171 +1,158 @@
-/**
-	Implementa el cuadro de progreso que informa del estado del procesamiento del fichero que se va a ejecutar
-	
-	@author Antonio Pérez Carrasco
-	@version 2006-2007
-*/
-
 package cuadros;
 
-
-
-
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.GridLayout;
 
 import javax.swing.border.TitledBorder;
-import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
-import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 import conf.*;
-import botones.*;
 import utilidades.*;
-import ventanas.*;
+import ventanas.Ventana;
 
+/**
+ * Implementa el cuadro de progreso que informa del estado de un determinado
+ * proceso de carga.
+ * 
+ * @author Antonio Pérez Carrasco
+ * @version 2006-2007
+ */
+public class CuadroProgreso extends Thread {
 
+	private static final int ANCHO_CUADRO = 450;
+	private static final int ALTO_CUADRO = 110;
 
-public class CuadroProgreso extends Thread //implements ActionListener
-{
-	static final long serialVersionUID=42;
+	private JLabel etiqueta;
+	private JPanel panel, panelTexto, panelProgreso;
+	private JProgressBar barra;
 
-	static final boolean depurar=false;
+	private BorderLayout bl;
+	private GridLayout gl;
 
-	final int anchoCuadro=450;
-	final int altoCuadro=110;
+	private JDialog dialogo;
 
-	JLabel etiqueta;
-	JPanel panel, panelTexto, panelProgreso;
-	JProgressBar barra;
-
-	BorderLayout bl;
-	GridLayout gl;
-	JDialog dialogo;
-	JFrame referenciaVentana;
-	
-	String nombre, texto;
-	int valor;
+	private String nombre, texto;
+	private int valor;
 
 	/**
-		Constructor: crea un nuevo cuadro de progreso
-		
-		@param ventanaVisualizador Ventana a la que quedará asociado
-		@param nombre Título que aparecerá en el cuadro
-		@param texto texto que se colocará en primera instancia en la etiqueta del panel
-		@param valor valor inicial que se asignará a la barra
-	*/
-	public CuadroProgreso(JFrame ventanaVisualizador,String nombre, String texto, int valor)
-	{	
-		this.dialogo=new JDialog(ventanaVisualizador,true);
-		//this.setAlwaysOnTop(true);
+	 * Genera un nuevo cuadro de progreso.
+	 * 
+	 * @param Ventana
+	 *            Ventana a la que quedará asociado el cuadro.
+	 * @param nombre
+	 *            Título que aparecerá en el cuadro
+	 * @param texto
+	 *            texto que se colocará en primera instancia en la etiqueta del
+	 *            panel
+	 * @param valor
+	 *            valor inicial que se asignará a la barra
+	 */
+	public CuadroProgreso(Ventana ventana, String nombre, String texto,
+			int valor) {
 
-		// Inicializamos atributos del cuadro de parámetros
-		
-		this.nombre=nombre;
-		this.texto=texto;
-		this.valor=valor;
-		this.etiqueta=new JLabel();
-		
-		this.barra = new JProgressBar(JProgressBar.HORIZONTAL,0,100);
-		barra.setSize(300,35);
+		this.dialogo = new JDialog(ventana, true);
+		this.nombre = nombre;
+		this.texto = texto;
+		this.valor = valor;
+		this.etiqueta = new JLabel();
+
+		this.barra = new JProgressBar(SwingConstants.HORIZONTAL, 0, 100);
+		this.barra.setSize(300, 35);
 		this.start();
 	}
-		
-	public void run()
-	{
+
+	/**
+	 * Ejecuta el thread asociado al cuadro.
+	 */
+	@Override
+	public void run() {
+
 		// Panel de texto
-		panelTexto = new JPanel();
-		panelTexto.add(etiqueta);
-		panelTexto.setBorder(new TitledBorder(Texto.get("CP_PROGPROC",Conf.idioma)));
-		panelTexto.setSize(320,80);
+		this.panelTexto = new JPanel();
+		this.panelTexto.add(this.etiqueta);
+		this.panelTexto.setBorder(new TitledBorder(Texto.get("CP_PROGPROC",
+				Conf.idioma)));
+		this.panelTexto.setSize(320, 80);
 
 		// Panel de progreso
-		gl = new GridLayout(0,1);
-		panelProgreso = new JPanel();
-		panelProgreso.setLayout(gl);
-		//panelProgreso.setSize(320,35);
-		panelProgreso.add(this.barra);
-		panelProgreso.setBorder(new TitledBorder(""));
-
+		this.gl = new GridLayout(0, 1);
+		this.panelProgreso = new JPanel();
+		this.panelProgreso.setLayout(this.gl);
+		this.panelProgreso.add(this.barra);
+		this.panelProgreso.setBorder(new TitledBorder(""));
 
 		// Panel general
-		bl= new BorderLayout();
-		panel = new JPanel();
-		panel.setLayout(bl);
+		this.bl = new BorderLayout();
+		this.panel = new JPanel();
+		this.panel.setLayout(this.bl);
 
-		panel.add(panelProgreso,BorderLayout.SOUTH);
-		panel.add(panelTexto,BorderLayout.NORTH);
-		
+		this.panel.add(this.panelProgreso, BorderLayout.SOUTH);
+		this.panel.add(this.panelTexto, BorderLayout.NORTH);
+
 		// Inicializacion de valores
-		setValores(texto,valor);
+		setValores(this.texto, this.valor);
 
-		this.dialogo.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		
+		this.dialogo
+				.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+
 		// Preparamos y mostramos cuadro
-		this.dialogo.getContentPane().add(panel);
-		this.dialogo.setTitle(nombre);
-		int coord[]=Conf.ubicarCentro(anchoCuadro,altoCuadro);
-		this.dialogo.setLocation(coord[0],coord[1]);
-		this.dialogo.setSize(anchoCuadro,altoCuadro);
+		this.dialogo.getContentPane().add(this.panel);
+		this.dialogo.setTitle(this.nombre);
+		int coord[] = Conf.ubicarCentro(ANCHO_CUADRO, ALTO_CUADRO);
+		this.dialogo.setLocation(coord[0], coord[1]);
+		this.dialogo.setSize(ANCHO_CUADRO, ALTO_CUADRO);
 		this.dialogo.setResizable(false);
 		this.dialogo.setVisible(true);
 	}
 
-
-	
 	/**
-		Asigna valores a la etiqueta y a la barra
-		
-		@param texto texto que aparecerá en la etiqueta
-		@param valor valor que quedará representado en la barra
-	*/
-	public void setValores(String texto, int valor)
-	{
-		etiqueta.setText(texto);
-		if (valor>=barra.getMinimum() && valor<=barra.getMaximum())
-			barra.setValue(valor);
+	 * Asigna valores a la etiqueta y a la barra
+	 * 
+	 * @param texto
+	 *            texto que aparecerá en la etiqueta
+	 * @param valor
+	 *            valor que quedará representado en la barra
+	 */
+	public void setValores(String texto, int valor) {
+		this.etiqueta.setText(texto);
+		if (valor >= this.barra.getMinimum()
+				&& valor <= this.barra.getMaximum()) {
+			this.barra.setValue(valor);
+		}
 	}
 
 	/**
-		Asigna valor a la etiqueta
-		
-		@param texto texto que aparecerá en la etiqueta
-	*/
-	public void setValores(String texto)
-	{
+	 * Asigna valor a la etiqueta.
+	 * 
+	 * @param texto
+	 *            texto que aparecerá en la etiqueta
+	 */
+	public void setValores(String texto) {
 		this.etiqueta.setText(texto);
 	}
 
 	/**
-		Asigna valor a la barra
-		
-		@param valor valor que quedará representado en la barra
-	*/
-	public void setValores(int valor)
-	{
-		if (valor>=barra.getMinimum() && valor<=barra.getMaximum())
-			barra.setValue(valor);
+	 * Asigna valor a la barra
+	 * 
+	 * @param valor
+	 *            valor que quedará representado en la barra
+	 */
+	public void setValores(int valor) {
+		if (valor >= this.barra.getMinimum()
+				&& valor <= this.barra.getMaximum()) {
+			this.barra.setValue(valor);
+		}
 	}
 
 	/**
-		Hace invisible el cuadro, una vez ha finalizado el procesamiento
-	*/
-	public void cerrar()
-	{
+	 * Hace invisible el cuadro, una vez ha finalizado el procesamiento
+	 */
+	public void cerrar() {
 		this.dialogo.setVisible(false);
-	}
-
-	/**
-		Hace invisible el cuadro, una vez ha finalizado el procesamiento
-	*/
-	public void abrir()
-	{
-		this.dialogo.setVisible(true);
 	}
 }
