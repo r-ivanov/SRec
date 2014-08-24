8018637bbb6ddc51b6a37fcfef1409f6d03a39a2
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-08-24 15:26:31 +0200

    Formato, Documentación y limpieza de código para la clase OpcionIdioma

--- opciones/OpcionIdioma.java
+++ opciones/OpcionIdioma.java
@@ -1,105 +1,98 @@
-/**
-	Opción que permite la configuración de mantenimiento de ficheros intermedios usados por la aplicación
-	
-	@author Antonio Pérez Carrasco
-	@version 2006-2007
-*/
-
 package opciones;
 
-import java.io.File;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import java.io.IOException;
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import utilidades.ManipulacionElement;
 
-import utilidades.*;
-
-public class OpcionIdioma extends Opcion
-{
+/**
+ * Opción que permite la configuración del idioma de la aplicación
+ * 
+ * @author Antonio Pérez Carrasco
+ * @version 2006-2007
+ */
+public class OpcionIdioma extends Opcion {
 	private static final long serialVersionUID = 1171;
 
-
-	String idioma;			
-
+	private String idioma;
 
 	/**
-		Constructor: genera una nueva opción con los valores por defecto
-	*/
-	public OpcionIdioma()
-	{
+	 * Constructor: genera una nueva opción con el idioma español por defecto.
+	 */
+	public OpcionIdioma() {
 		this("es");
 	}
 
 	/**
-		Constructor: genera una nueva opción con los valores dados con parámetros
-		
-		@param idiomaId código de idioma
-	*/
-	public OpcionIdioma(String idiomaId)
-	{
+	 * Constructor: genera una nueva opción con el idioma pasado por parámetro.
+	 * 
+	 * @param idiomaId
+	 *            código de idioma
+	 */
+	public OpcionIdioma(String idiomaId) {
 		super("OpcionIdioma");
-		this.idioma=idiomaId;
+		this.idioma = idiomaId;
 	}
 
 	/**
-		Almacena los valores de la opción
-		
-		@param idiomaId código de idioma
-	*/
-	public void set(String idiomaId)
-	{
-		this.idioma=idiomaId;
+	 * Permite establecer el idioma.
+	 * 
+	 * @param idiomaId
+	 *            código de idioma
+	 */
+	public void set(String idiomaId) {
+		this.idioma = idiomaId;
 	}
 
-	
 	/**
-		Almacena los valores de la opción
-		
-		@param idiomaId código de idioma
-	*/
-	public String get()
-	{
+	 * Devuelve el idioma a utilizar.
+	 * 
+	 * @return idiomaId
+	 *            código de idioma
+	 */
+	public String get() {
 		return this.idioma;
 	}
-	
-	
-	public Element getRepresentacionElement(Document d)
-	{
-		Element e=d.createElement("OpcionIdioma");
-		
-		Element e01=d.createElement("idioma");
-		e01.setAttribute("valor",this.idioma);
-		
+
+	@Override
+	public Element getRepresentacionElement(Document d) {
+		Element e = d.createElement("OpcionIdioma");
+
+		Element e01 = d.createElement("idioma");
+		e01.setAttribute("valor", this.idioma);
+
 		e.appendChild(e01);
-	
+
 		return e;
 	}
-	
-	
-	public void setValores(Element e)
-	{
-		Element elements[]=ManipulacionElement.nodeListToElementArray(e.getElementsByTagName("idioma"));
-		this.idioma=elements[0].getAttribute("valor");
-	}
-	
 
-	
-	/**
-		Gestiona la lectura desde fichero
-	*/
-	private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException
-	{
-		this.idioma=stream.readUTF();
+	@Override
+	public void setValores(Element e) {
+		Element elements[] = ManipulacionElement.nodeListToElementArray(e
+				.getElementsByTagName("idioma"));
+		this.idioma = elements[0].getAttribute("valor");
 	}
 
 	/**
-		Gestiona la escritura a fichero
-	*/
-	private void writeObject(ObjectOutputStream stream) throws IOException
-	{
+	 * Gestiona la lectura desde fichero
+	 * 
+	 * @param stream Input stream para el fichero.
+	 */
+	private void readObject(ObjectInputStream stream) throws IOException,
+	ClassNotFoundException {
+		this.idioma = stream.readUTF();
+	}
+
+	/**
+	 * Gestiona la escritura a fichero
+	 * 
+	 * @param stream Output stream para el fichero.
+	 */
+	private void writeObject(ObjectOutputStream stream) throws IOException {
 		stream.writeUTF(this.idioma);
 	}
 }
