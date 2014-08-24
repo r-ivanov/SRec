a7e0864bc9a7368b4da61a8734f1a9dabba2031a
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-08-24 19:50:22 +0200

    Formato, Documentación y limpieza de código para la clase PanelCompilador.

--- paneles/PanelCompilador.java
+++ paneles/PanelCompilador.java
@@ -1,12 +1,4 @@
-/**
-	Panel que contendrá la visualización del algoritmo, ya que almacena la propia traza.
-	Está contenido en PanelAlgoritmo.
-	
-	@author Antonio Pérez Carrasco
-	@version 2006-2007
-*/
 package paneles;
-
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -15,209 +7,183 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
-
 import javax.swing.JTextPane;
 
 import utilidades.Texto;
-import ventanas.Ventana;
+import conf.Conf;
 
+/**
+ * Panel que contiene los resultados de compilación de una determinada clase.
+ * 
+ * @author Antonio Pérez Carrasco
+ * @version 2006-2007
+ */
+public class PanelCompilador implements MouseListener {
+	static final long serialVersionUID = 04;
 
+	private JTextPane espacioCompilador;
 
+	private JPanel panel = new JPanel();
 
+	private PanelAlgoritmo pAlgoritmo;
 
-import conf.*;
+	public static final String CODIGO_VACIO = "      ";
 
+	private final int LONG_MINIMA = CODIGO_VACIO.length() - 1;
 
-public class PanelCompilador implements MouseListener
-{
-	static final long serialVersionUID=04;
+	/**
+	 * Constructor: Crea un nuevo PanelCompilador.
+	 * 
+	 * @param pAlgoritmo PanelAlgoritmo que contendrá este panel.
+	 */
+	public PanelCompilador(PanelAlgoritmo pAlgoritmo) {
+		this.pAlgoritmo = pAlgoritmo;
 
-	String texto;
-	
-	StringBuffer contents;
+		this.espacioCompilador = new JTextPane();
+		this.espacioCompilador.setText("");
+		this.espacioCompilador.setFont(new Font("Courier New", Font.PLAIN, 11));
+		this.espacioCompilador.setBackground(Conf.colorPanel);
+		this.espacioCompilador.setEditable(false);
+		this.espacioCompilador.addMouseListener(this);
 
-	JTextPane espacioCompilador;
-	PanelEditorJava panelJava;
-	
-	JScrollPane contenedorPanel;
-	
-	JPanel panel=new JPanel();
-	
-	//PanelCodigo pCodigo;
-	PanelAlgoritmo pAlgoritmo;
-	
-	public static final String CODIGO_VACIO="      ";
-	
-	
-	final int LONG_MINIMA=CODIGO_VACIO.length()-1;
-	
+		this.panel.setLayout(new BorderLayout());
+		this.panel.add(this.espacioCompilador);
+
+		this.borrarTextoCompilador();
+		this.panel.updateUI();
+
+	}
 	
 	/**
-		Constructor: crea un nuevo panel de traza
-		
-		@param numPanelesTraza número de panales que se desean
-	*/
-	public PanelCompilador(PanelAlgoritmo pAlgoritmo)
-	{
-		this.texto="";
-		this.pAlgoritmo=pAlgoritmo;
-		//this.pCodigo=pCodigo;
-		
-		
-		espacioCompilador = new JTextPane();
-		espacioCompilador.setText("");
-		espacioCompilador.setFont(new Font("Courier New",Font.PLAIN, 11));
-		espacioCompilador.setBackground(Conf.colorPanel);
-		espacioCompilador.setEditable(false);
-		espacioCompilador.addMouseListener(this);
-		
-		panel.setLayout(new BorderLayout());
-		panel.add(espacioCompilador);
-		
-		this.borrarTextoCompilador();
-		panel.updateUI();
-		
-		
-	}
-	
-	
-	public JPanel getPanel()
-	{
+	 * Devuelve el panel interno.
+	 * 
+	 * @return Panel.
+	 */
+	public JPanel getPanel() {
 		return this.panel;
 	}
-	
-
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource()==espacioCompilador)
-		{
-			new Thread(){
-				public synchronized void run()
-				{
-					try { wait(60); } catch (Exception e) {};
-					String seleccionado=espacioCompilador.getSelectedText();
-					if (seleccionado!=null)
-					{
-						int numeroLinea=numLinea(seleccionado);
-						//System.out.println("LineaDeCodigo="+numeroLinea);
-						
-						int nLineas=numeroLinea;
-						
-						if (numeroLinea!=-1)
-						{
-							int longitud=0;
-							String textoareatexto=pAlgoritmo.getPanelCodigo().getText();
+		if (e.getSource() == this.espacioCompilador) {
+			new Thread() {
+				@Override
+				public synchronized void run() {
+					try {
+						this.wait(60);
+					} catch (Exception e) {
+					}
+					;
+					String seleccionado = PanelCompilador.this.espacioCompilador
+							.getSelectedText();
+					if (seleccionado != null) {
+						int numeroLinea = PanelCompilador.this
+								.numLinea(seleccionado);
 
-							//System.out.println("numeroLinea = "+numeroLinea);
-							while (numeroLinea>1)		// !=0
+						int nLineas = numeroLinea;
+
+						if (numeroLinea != -1) {
+							int longitud = 0;
+							String textoareatexto = PanelCompilador.this.pAlgoritmo
+									.getPanelCodigo().getText();
+
+							while (numeroLinea > 1) // !=0
 							{
-								longitud=longitud+(textoareatexto.substring(0,textoareatexto.indexOf("\n")).length()+1);
-								//System.out.println("["+textoareatexto.substring(0,textoareatexto.indexOf("\n"))+"]");
-								textoareatexto=textoareatexto.substring(textoareatexto.indexOf("\n")+1,
-													textoareatexto.length());
+								longitud = longitud
+										+ (textoareatexto.substring(0,
+												textoareatexto.indexOf("\n"))
+												.length() + 1);
+								textoareatexto = textoareatexto.substring(
+										textoareatexto.indexOf("\n") + 1,
+										textoareatexto.length());
 								numeroLinea--;
-								//System.out.println("longitud = "+longitud);
 							}
-							int inicio=longitud;
-							
+							int inicio = longitud;
+
 							try {
-								longitud=(textoareatexto.substring(0,textoareatexto.indexOf("\n")).length());
+								longitud = (textoareatexto.substring(0,
+										textoareatexto.indexOf("\n")).length());
 							} catch (Exception e) {
-								longitud=textoareatexto.length();
+								longitud = textoareatexto.length();
 							}
-							//System.out.println("longitud = "+longitud+" ["+textoareatexto.substring(0,textoareatexto.indexOf("\n"))+"]");
 							
-							
-							pAlgoritmo.getPanelCodigo().select(inicio-nLineas+1,longitud);
-							
-							//System.out.println("inicio="+inicio+"  final="+longitud);
+							PanelCompilador.this.pAlgoritmo.getPanelCodigo()
+									.select(inicio - nLineas + 1, longitud);
 						}
 					}
 				}
 			}.start();
 		}
 	}
-	
-	
-	private int numLinea(String texto)
-	{
-		if ( ( texto.indexOf("java:") )>=0 && ( texto.indexOf("java:")+5 )<texto.length())
-		{
-			String t=texto.substring( texto.indexOf("java:")+5,texto.length());
-			
-			if (t.indexOf(":")!=-1)
-			{
-				t=t.substring(0,t.indexOf(":"));
+
+	private int numLinea(String texto) {
+		if ((texto.indexOf("java:")) >= 0
+				&& (texto.indexOf("java:") + 5) < texto.length()) {
+			String t = texto.substring(texto.indexOf("java:") + 5,
+					texto.length());
+
+			if (t.indexOf(":") != -1) {
+				t = t.substring(0, t.indexOf(":"));
 			}
-			int x=-1;
+			int x = -1;
 			try {
-				x=Integer.parseInt(t);
+				x = Integer.parseInt(t);
 			} catch (Exception e) {
 				return -1;
 			}
 			return x;
-			}
+		}
 		return -1;
 	}
-
 	
-	
-	public void borrarTextoCompilador()
-	{
+	/**
+	 * Permite borrar el texto mostrado.
+	 */
+	public void borrarTextoCompilador() {
 		this.setTextoCompilador(CODIGO_VACIO);
 	}
 	
-	public void setTextoCompilador(String texto)
-	{
-		
-		if (texto.length()<LONG_MINIMA)
-		{
+	/**
+	 * Permite establecer el texto mostrado.
+	 */
+	public void setTextoCompilador(String texto) {
+
+		if (texto.length() < this.LONG_MINIMA) {
 			this.espacioCompilador.setBackground(Conf.colorPanel);
-			this.espacioCompilador.setText(Texto.get("COMPILAR_NOERRORES",Conf.idioma));
-		}
-		else if (texto.equals(CODIGO_VACIO))
-		{
-			this.espacioCompilador.setBackground(new Color(255,255,255));
+			this.espacioCompilador.setText(Texto.get("COMPILAR_NOERRORES",
+					Conf.idioma));
+		} else if (texto.equals(CODIGO_VACIO)) {
+			this.espacioCompilador.setBackground(new Color(255, 255, 255));
 			this.espacioCompilador.setText(texto);
-		}
-		else
-		{
+		} else {
 			this.espacioCompilador.setBackground(Conf.colorPanel);
 			this.espacioCompilador.setText(texto);
 		}
-		
+
 	}
 }
-
