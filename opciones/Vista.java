527479a245204d89f920ad6383fb6b82a49e217a
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-08-24 17:04:37 +0200

    Formato, Documentación y limpieza de código para la clase Vista.

--- opciones/Vista.java
+++ opciones/Vista.java
@@ -1,105 +1,129 @@
-
-
-
 package opciones;
 
-
-import org.w3c.dom.*;
-
+/**
+ * Representa una vista de ejecución del algoritmo.
+ */
 public class Vista {
 
-	final public static int TIPO_REC=1;
-	final public static int TIPO_DYV=2;
-	
-	final public static String codigos[]={	"V_ARBOL",	"V_PILA",	"V_TRAZA",	"V_ESTRUC"};
-									//			0			1			2			3	
-	String codigo;
-	
-	boolean activa=false;
-	
-	int tipo=Vista.TIPO_REC;
-	
-	int panel=1;
-	
-	public Vista(String codigo)
-	{
-		this.codigo=codigo;
-		
+	public static final int TIPO_REC = 1;
+	public static final int TIPO_DYV = 2;
+
+	public static final String codigos[] = { "V_ARBOL", "V_PILA", "V_TRAZA",
+			"V_ESTRUC" };
+	private String codigo;
+
+	private boolean activa = false;
+
+	private int tipo = Vista.TIPO_REC;
+
+	private int panel = 1;
+
+	/**
+	 * Crea una nueva vista, dado uno de los códigos especificados en
+	 * Vista.codigos.
+	 * 
+	 * @param codigo
+	 *            Algún código almacenado en Vista.codigos.
+	 */
+	public Vista(String codigo) {
+		this.codigo = codigo;
 	}
-	
-	
-	public String getCodigo()
-	{
+
+	/**
+	 * Devuelve el código que identifica a la vista.
+	 * 
+	 * @return Algún código almacenado en Vista.codigos.
+	 */
+	public String getCodigo() {
 		return this.codigo;
 	}
-	
-	public void setActiva(boolean valor)
-	{
-		this.activa=valor;
+
+	/**
+	 * Establece la vista como activa.
+	 * 
+	 * @param valor
+	 *            true para estar activa, false en caso contrario.
+	 */
+	public void setActiva(boolean valor) {
+		this.activa = valor;
 	}
-	
-	public boolean esActiva()
-	{
+
+	/**
+	 * Devuelve si la vista está activa o no.
+	 * 
+	 * @return true si la vista está activa, false en caso contrario.
+	 */
+	public boolean esActiva() {
 		return this.activa;
 	}
-	
-	public void setTipo(int tipo)
-	{
-		switch(tipo)
-		{
-			case Vista.TIPO_REC:
-				this.tipo=TIPO_REC;
-				break;
-			case Vista.TIPO_DYV:
-				this.tipo=TIPO_DYV;
-				break;
-			default:
-				this.tipo=TIPO_REC;
+
+	/**
+	 * Permite establecer el tipo de la vista dependiendo de el tipo de
+	 * ejecución.
+	 * 
+	 * @param tipo
+	 *            TIPO_REC o TIPO_DYV
+	 */
+	public void setTipo(int tipo) {
+		switch (tipo) {
+		case Vista.TIPO_REC:
+			this.tipo = TIPO_REC;
+			break;
+		case Vista.TIPO_DYV:
+			this.tipo = TIPO_DYV;
+			break;
+		default:
+			this.tipo = TIPO_REC;
 		}
 	}
-	
-	public int getTipo()
-	{
+
+	/**
+	 * Devuelve el tipo de la vista.
+	 * 
+	 * @return TIPO_REC o TIPO_DYV
+	 */
+	public int getTipo() {
 		return this.tipo;
 	}
-	
-	public void setPanel(int x)
-	{
-		if (x==2)
-			this.panel=2;
-		else
-			this.panel=1;
+
+	/**
+	 * Establece la vista como parte de uno de los dos paneles.
+	 * 
+	 * @param x
+	 *            1 para el panel 1, 2 para el panel 2.
+	 */
+	public void setPanel(int x) {
+		if (x == 2) {
+			this.panel = 2;
+		} else {
+			this.panel = 1;
+		}
 	}
-	
-	public int getPanel()
-	{
+
+	/**
+	 * Devuelve el panel en el que se encuentra la vista.
+	 * 
+	 * @return 1, o 2.
+	 */
+	public int getPanel() {
 		return this.panel;
 	}
-	
-	
-	
-	public Element getRepresentacionElement(Document d)
-	{
-		Element e=d.createElement("Vista");
-		
-		e.setAttribute("codigo", this.getCodigo());
-		e.setAttribute("activa", ""+this.esActiva());
-		e.setAttribute("tipo", ""+this.getTipo());
-		e.setAttribute("panel", ""+this.getPanel());
-		
-		
-		return e;
-	}
-	
-	
-	
-	public static int getPosic(String nombre)
-	{
-		for (int i=0; i<codigos.length; i++)
-			if (nombre.equals(codigos[i]))
-				return i;
 
+	/**
+	 * Permite obtener la posición del código de vista dentro de Vista.codigos.
+	 * 
+	 * @param nombre
+	 *            Código de la vista.
+	 * 
+	 * @return posición, o -1 si el código no existe.
+	 */
+	public static int getPosic(String nombre) {
+		for (int i = 0; i < codigos.length; i++) {
+			if (nombre.equals(codigos[i])) {
+				return i;
+			}
+		}
 		return -1;
 	}
-	
+
 }
