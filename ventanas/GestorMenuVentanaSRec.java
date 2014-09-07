9303c802ec9c0c8941c2868929e271bfb01f4730
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-09-07 19:18:30 +0200

    Formato, Documentación y limpieza de código para la clase GestorMenuVentanaSRec

--- ventanas/GestorMenuVentanaSRec.java
+++ ventanas/GestorMenuVentanaSRec.java
@@ -7,31 +7,32 @@ import javax.swing.event.MenuListener;
 import utilidades.Logger;
 import conf.Conf;
 
+/**
+ * Gestiona los eventos en los menús de la Ventana principal.
+ */
 public class GestorMenuVentanaSRec implements MenuListener {
-
 
 	@Override
 	public void menuCanceled(MenuEvent e) {
-		// TODO Auto-generated method stub
-		if (Conf.fichero_log) Logger.log_write("Menú cancelado: "+((JMenu)(e.getSource())).getText());
+		if (Conf.fichero_log) {
+			Logger.log_write("Menú cancelado: "
+					+ ((JMenu) (e.getSource())).getText());
+		}
 	}
-
-
-
 
 	@Override
 	public void menuDeselected(MenuEvent e) {
-		// TODO Auto-generated method stub
-		if (Conf.fichero_log) Logger.log_write("Menú deseleccionado: "+((JMenu)(e.getSource())).getText());
+		if (Conf.fichero_log) {
+			Logger.log_write("Menú deseleccionado: "
+					+ ((JMenu) (e.getSource())).getText());
+		}
 	}
-
-
-
 
 	@Override
 	public void menuSelected(MenuEvent e) {
-		// TODO Auto-generated method stub
-		if (Conf.fichero_log) Logger.log_write("Menú seleccionado: "+((JMenu)(e.getSource())).getText());
+		if (Conf.fichero_log) {
+			Logger.log_write("Menú seleccionado: "
+					+ ((JMenu) (e.getSource())).getText());
+		}
 	}
-
 }
