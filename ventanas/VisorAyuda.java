53f14ef90c541e0201c3df00ffc506a3dce6aced
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-09-07 21:49:22 +0200

    Formato, Documentación y limpieza de código para la clase VisorAyuda

--- ventanas/VisorAyuda.java
+++ ventanas/VisorAyuda.java
@@ -1,41 +1,34 @@
 package ventanas;
 
-
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
-
 import java.net.URL;
 
-import javax.swing.event.HyperlinkListener;
-import javax.swing.event.HyperlinkEvent;
-
-
 import javax.swing.ImageIcon;
-
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
 import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
+import utilidades.Texto;
+import botones.BotonImagen;
+import conf.Conf;
 
-
-import botones.*;
-import conf.*;
-import utilidades.*;
-
-public class VisorAyuda extends Thread implements ActionListener, MouseListener, KeyListener, HyperlinkListener
-{
+/**
+ * Ventana de ayuda de la aplicación.
+ */
+public class VisorAyuda extends Thread implements ActionListener,
+		MouseListener, KeyListener, HyperlinkListener {
 
 	private JFrame frame;
 	private JEditorPane pane;
@@ -46,366 +39,313 @@ public class VisorAyuda extends Thread implements ActionListener, MouseListener,
 	private BotonImagen atras;
 	private BotonImagen adelante;
 	private BotonImagen indice;
-	
-	String pilaAt[]=new String[0];	// Pila para ir hacia atrás
-	String pilaAd[]=new String[0];	// Pila para ir hacia adelante
 
+	private String pilaAt[] = new String[0]; // Pila para ir hacia atrás
+	private String pilaAd[] = new String[0]; // Pila para ir hacia adelante
+
+	private final int anchoCuadro = 750;
+	private final int altoCuadro = 570;
 	
-	final int anchoCuadro=750;
-	final int altoCuadro=570;
-	
-	
-	
-	public VisorAyuda()
-	{
+	/**
+	 * Construye un nuevo Visor de ayuda cargando el fichero html principal.
+	 */
+	public VisorAyuda() {
 		this("index.html");
 	}
 	
-	public VisorAyuda(String s)
-	{
-		this.fichero=s;
+	/**
+	 * Construye un nuevo Visor de ayuda cargando el fichero pasado por parámetro.
+	 * 
+	 * @param s Fichero a visualizar
+	 */
+	public VisorAyuda(String s) {
+		this.fichero = s;
 		this.start();
 	}
-	
-	public void run()
-	{
-		frame=new JFrame(Texto.get("VA_AYUDA",Conf.idioma));
-		
-		//Botones
-		atras = new BotonImagen("imagenes/boton_atrasayuda_verde.gif","imagenes/boton_atrasayuda_naranja.gif",
-					"imagenes/boton_atrasayuda_rojo.gif", Conf.botonVisualizacionAncho,Conf.botonVisualizacionAlto);
-		adelante = new BotonImagen("imagenes/boton_adelanteayuda_verde.gif","imagenes/boton_adelanteayuda_naranja.gif",
-					"imagenes/boton_adelanteayuda_rojo.gif", Conf.botonVisualizacionAncho,Conf.botonVisualizacionAlto);
-		indice = new BotonImagen("imagenes/boton_indice_verde.gif","imagenes/boton_indice_naranja.gif",
-					"imagenes/boton_indice_rojo.gif",Conf.botonVisualizacionAncho,Conf.botonVisualizacionAlto);
 
-		atras.addMouseListener(this);
-		adelante.addMouseListener(this);
-		indice.addMouseListener(this);
-		
-		atras.setToolTipText(Texto.get("VA_ATRAS",Conf.idioma));
-		adelante.setToolTipText(Texto.get("VA_ADEL",Conf.idioma));
-		indice.setToolTipText(Texto.get("VA_INDICE",Conf.idioma));
-		
-		
+	@Override
+	public void run() {
+		this.frame = new JFrame(Texto.get("VA_AYUDA", Conf.idioma));
+
+		// Botones
+		this.atras = new BotonImagen("imagenes/boton_atrasayuda_verde.gif",
+				"imagenes/boton_atrasayuda_naranja.gif",
+				"imagenes/boton_atrasayuda_rojo.gif",
+				Conf.botonVisualizacionAncho, Conf.botonVisualizacionAlto);
+		this.adelante = new BotonImagen(
+				"imagenes/boton_adelanteayuda_verde.gif",
+				"imagenes/boton_adelanteayuda_naranja.gif",
+				"imagenes/boton_adelanteayuda_rojo.gif",
+				Conf.botonVisualizacionAncho, Conf.botonVisualizacionAlto);
+		this.indice = new BotonImagen("imagenes/boton_indice_verde.gif",
+				"imagenes/boton_indice_naranja.gif",
+				"imagenes/boton_indice_rojo.gif", Conf.botonVisualizacionAncho,
+				Conf.botonVisualizacionAlto);
+
+		this.atras.addMouseListener(this);
+		this.adelante.addMouseListener(this);
+		this.indice.addMouseListener(this);
+
+		this.atras.setToolTipText(Texto.get("VA_ATRAS", Conf.idioma));
+		this.adelante.setToolTipText(Texto.get("VA_ADEL", Conf.idioma));
+		this.indice.setToolTipText(Texto.get("VA_INDICE", Conf.idioma));
+
 		JPanel panelSuperior = new JPanel();
-		BorderLayout bl= new BorderLayout();
+		BorderLayout bl = new BorderLayout();
 		panelSuperior.setLayout(bl);
-		JPanel panelBotones=new JPanel();
-		
-		panelBotones.add(atras);
-		panelBotones.add(adelante);
-		panelBotones.add(indice);
-		panelSuperior.add(panelBotones,BorderLayout.WEST);
-		frame.getContentPane().add(panelSuperior, "North");
-		
-		File f=new File("");
-		this.fichero="file:///"+f.getAbsolutePath()+"\\ayuda\\"+this.fichero;
-		this.index="file:///"+f.getAbsolutePath()+"\\ayuda\\"+Conf.idioma+"_index.html";
-		//System.out.println("["+fichero+"]");
-		
-		pane = new JEditorPane();
-		pane.setEditable(false); 
-		frame.getContentPane().add(new JScrollPane(pane), "Center");
+		JPanel panelBotones = new JPanel();
+
+		panelBotones.add(this.atras);
+		panelBotones.add(this.adelante);
+		panelBotones.add(this.indice);
+		panelSuperior.add(panelBotones, BorderLayout.WEST);
+		this.frame.getContentPane().add(panelSuperior, "North");
+
+		File f = new File("");
+		this.fichero = "file:///" + f.getAbsolutePath() + "\\ayuda\\"
+				+ this.fichero;
+		this.index = "file:///" + f.getAbsolutePath() + "\\ayuda\\"
+				+ Conf.idioma + "_index.html";
+
+		this.pane = new JEditorPane();
+		this.pane.setEditable(false);
+		this.frame.getContentPane().add(new JScrollPane(this.pane), "Center");
 		try {
-			pane.setPage(this.index);
+			this.pane.setPage(this.index);
 		} catch (IOException e) {
 			try {
-				pane.setPage("file:///"+f.getAbsolutePath()+"\\ayuda\\"+"error.html");
+				this.pane.setPage("file:///" + f.getAbsolutePath()
+						+ "\\ayuda\\" + "error.html");
 			} catch (IOException e2) {
 				System.out.println("VisorAyuda fichero no encontrado");
 			}
 		}
-		//anadirAt(this.fichero);
-		//mostrar(pilaAt,"\n\npilaAt");
-		//mostrar(pilaAd,"\n\npilaAd");
-		
-		adelante.deshabilitar();
-		atras.deshabilitar();
-			
-		frame.setIconImage( new ImageIcon("./imagenes/icono_ayuda.gif").getImage() );
-		frame.setVisible(true);
-		frame.setResizable(false);
-		int coord[]=Conf.ubicarCentro(anchoCuadro,altoCuadro);
-		frame.setLocation(coord[0],coord[1]);
-		frame.setSize(anchoCuadro, altoCuadro);
-		//ActivatedHyperlinkListener listener=new ActivatedHyperlinkListener(frame,pane);
-		pane.addHyperlinkListener( this );
 
-	}
-	
-		
-	/**
-		Gestiona los eventos de acción
-		
-		@param e evento de acción
-	*/	
-	public void actionPerformed(ActionEvent e)
-	{
-		
-	}
-	
-	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/		
-	public void keyPressed(KeyEvent e)
-	{
-	}
-	
-	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/			
-	public void keyReleased(KeyEvent e)
-	{
-	}	
+		this.adelante.deshabilitar();
+		this.atras.deshabilitar();
 
-	
-	/**
-		Gestiona los eventos de teclado
-		
-		@param e evento de teclado
-	*/			
-	public void keyTyped(KeyEvent e)
-	{
-	}	
-		
-	
-	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/			
-	public void mouseClicked(MouseEvent e) 
-	{
-		colorearBotonesAdelanteAtras();
-	}
-	
-	
-	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/			
-	public void mouseEntered(MouseEvent e) 
-	{
-		if (e.getSource() instanceof BotonImagen)
-			((BotonImagen)(e.getSource())).mostrarPulsado();		
+		this.frame.setIconImage(new ImageIcon("./imagenes/icono_ayuda.gif")
+				.getImage());
+		this.frame.setVisible(true);
+		this.frame.setResizable(false);
+		int coord[] = Conf.ubicarCentro(this.anchoCuadro, this.altoCuadro);
+		this.frame.setLocation(coord[0], coord[1]);
+		this.frame.setSize(this.anchoCuadro, this.altoCuadro);
+		this.pane.addHyperlinkListener(this);
 	}
 
-	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/			
-	public void mouseExited(MouseEvent e) 
-	{
-		if (e.getSource()==indice)
-			indice.habilitar();
-		colorearBotonesAdelanteAtras();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
 	}
 
-	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/			
-	public void mousePressed(MouseEvent e) 
-	{
-		if (e.getSource()==indice)
-		{
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		this.colorearBotonesAdelanteAtras();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		if (e.getSource() instanceof BotonImagen) {
+			((BotonImagen) (e.getSource())).mostrarPulsado();
+		}
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		if (e.getSource() == this.indice) {
+			this.indice.habilitar();
+		}
+		this.colorearBotonesAdelanteAtras();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getSource() == this.indice) {
 			// Reinicializamos cuál debe ser el index, por si ha cambiado idioma
-			File f=new File("");
-			this.index="file:///"+f.getAbsolutePath()+"\\ayuda\\"+Conf.idioma+"_index.html";
+			File f = new File("");
+			this.index = "file:///" + f.getAbsolutePath() + "\\ayuda\\"
+					+ Conf.idioma + "_index.html";
 			// Fin reinicializacion
-			
-			if (!this.fichero.equals(this.index))
-				anadirAt(this.fichero);
+
+			if (!this.fichero.equals(this.index)) {
+				this.anadirAt(this.fichero);
+			}
 			try {
-				pane.setPage(this.index);
+				this.pane.setPage(this.index);
 			} catch (IOException ioe) {
 				try {
-					pane.setPage("file:///"+f.getAbsolutePath()+"\\ayuda\\"+"error.html");
+					this.pane.setPage("file:///" + f.getAbsolutePath()
+							+ "\\ayuda\\" + "error.html");
 				} catch (IOException e2) {
 					System.out.println("VisorAyuda fichero no encontrado");
 				}
 			}
-			this.fichero=this.index;
-			
-			borrarAd();
-		}
-		else if (e.getSource()==atras)
-		{
-			anadirAd(this.fichero);
-			this.fichero=extraerAt();
+			this.fichero = this.index;
+
+			this.borrarAd();
+		} else if (e.getSource() == this.atras) {
+			this.anadirAd(this.fichero);
+			this.fichero = this.extraerAt();
 			try {
-				pane.setPage(this.fichero);
+				this.pane.setPage(this.fichero);
+			} catch (IOException ioe) {
+			}
+		} else if (e.getSource() == this.adelante) {
+			this.anadirAt(this.fichero);
+			this.fichero = this.extraerAd();
+			try {
+				this.pane.setPage(this.fichero);
 			} catch (IOException ioe) {
 			}
 		}
-		else if (e.getSource()==adelante)
-		{
-			anadirAt(this.fichero);
-			this.fichero=extraerAd();
-			try {
-				pane.setPage(this.fichero);
-			} catch (IOException ioe) {
-			}			
-		}
-		else
-		{
-			//System.out.println(e.getSource().getClass().getCanonicalName());
-		}
 	}
-	
-	/**
-		Gestiona los eventos de ratón
-		
-		@param e evento de ratón
-	*/			
-	public void mouseReleased(MouseEvent e)
-	{
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
 	}
-	
-	/**
-		Gestiona los eventos de hiperenlaces
-		
-		@param e evento de ratón
-	*/		
-	public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent)
-	{
+
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
 		HyperlinkEvent.EventType type = hyperlinkEvent.getEventType();
 		final URL url = hyperlinkEvent.getURL();
 		if (type == HyperlinkEvent.EventType.ENTERED) {
-			//System.out.println("URL: " + url);
-		}
-		else if (type == HyperlinkEvent.EventType.ACTIVATED)
-		{
-			anadirAt(this.fichero);
-			this.fichero=url.toExternalForm();
-			//System.out.println("Activated");
-			Runnable runner = new Runnable()
-			{
-				public void run()
-				{
+			// No hacer nada
+		} else if (type == HyperlinkEvent.EventType.ACTIVATED) {
+			this.anadirAt(this.fichero);
+			this.fichero = url.toExternalForm();
+			Runnable runner = new Runnable() {
+				@Override
+				public void run() {
 					// Retain reference to original
 					try {
-						pane.setPage(url);
+						VisorAyuda.this.pane.setPage(url);
 					} catch (IOException ioException) {
 						try {
-							File f=new File("");
-							pane.setPage("file:///"+f.getAbsolutePath()+"\\ayuda\\"+"error.html");
+							File f = new File("");
+							VisorAyuda.this.pane.setPage("file:///"
+									+ f.getAbsolutePath() + "\\ayuda\\"
+									+ "error.html");
 						} catch (IOException ioException2) {
-						
+
 						}
-						/*JOptionPane.showMessageDialog(frame,
-							"El archivo necesario no existe", "Enlace incorrecto",
-						JOptionPane.ERROR_MESSAGE);
-						pane.setDocument(doc);*/
 					}
 				}
 			};
 			SwingUtilities.invokeLater(runner);
-			
-			borrarAd();
-			colorearBotonesAdelanteAtras();
-			//mostrar(pilaAt,"\n\npilaAt");
-			//mostrar(pilaAd,"\n\npilaAd");
-		}
-	}
-	
-	/*private void borrarAt()
-	{
-		pilaAt=new String[0];
-	}*/
-	
-	private void borrarAd()
-	{
-		pilaAd=new String[0];
-	}
-	
-	private void anadirAt(String f)
-	{
-		if (pilaAt.length==0 || !(f.equals(pilaAt[0])))
-		{
-			String pilaAux[]=new String[pilaAt.length+1];
-			pilaAux[0]=f;
-			for (int i=0; i<pilaAt.length; i++)
-				pilaAux[i+1]=pilaAt[i];
-			pilaAt=pilaAux;
-		}
-	}
-	
-	private void anadirAd(String f)
-	{
-		if (pilaAd.length==0 || !(f.equals(pilaAd[0])))
-		{
-			String pilaAux[]=new String[pilaAd.length+1];
-			pilaAux[0]=f;
-			for (int i=0; i<pilaAd.length; i++)
-				pilaAux[i+1]=pilaAd[i];
-			pilaAd=pilaAux;
-		}
-	}
-	
-	private String extraerAt()
-	{
-		if (pilaAt.length>0)
-		{
-			String pos0=pilaAt[0];
-			String pilaAux[]=new String[pilaAt.length-1];
-			for (int i=0; i<pilaAux.length; i++)
-				pilaAux[i]=pilaAt[i+1];
-			pilaAt=pilaAux;
-			if (!(pos0.equals(this.fichero)))
-				return pos0;
-			else
-				return extraerAt();
-		}
-		else
-			return this.fichero;
-	}
-	
-	private String extraerAd()
-	{
-		if (pilaAd.length>0)
-		{
-			String pos0=pilaAd[0];
-			String pilaAux[]=new String[pilaAd.length-1];
-			for (int i=0; i<pilaAux.length; i++)
-				pilaAux[i]=pilaAd[i+1];
-			pilaAd=pilaAux;
-			if (!(pos0.equals(this.fichero)))
-				return pos0;
-			else
-				return extraerAd();	
-		}
-		else
-			return this.fichero;
-	}
-	
-	/*private void mostrar(String pila[],String mensaje)
-	{
-		//System.out.println(mensaje);
-		//for (int i=0; i<pila.length; i++)
-		//	System.out.println(" - "+pila[i]);
-		//System.out.println("fin pila ("+pila.length+" posiciones)");*
-	}*/
-	
-	private void colorearBotonesAdelanteAtras()
-	{
-		if (pilaAt.length>0)
-			atras.habilitar();
-		else
-			atras.deshabilitar();
 
-		if (pilaAd.length>0)
-			adelante.habilitar();
-		else
-			adelante.deshabilitar();
+			this.borrarAd();
+			this.colorearBotonesAdelanteAtras();
+		}
+	}
+	
+	/**
+	 * Elimina los elementos disponibles en la pila de páginas posteriores.
+	 */
+	private void borrarAd() {
+		this.pilaAd = new String[0];
+	}
+	
+	/**
+	 * Añade un elemento en la pila de páginas anteriores.
+	 */
+	private void anadirAt(String f) {
+		if (this.pilaAt.length == 0 || !(f.equals(this.pilaAt[0]))) {
+			String pilaAux[] = new String[this.pilaAt.length + 1];
+			pilaAux[0] = f;
+			for (int i = 0; i < this.pilaAt.length; i++) {
+				pilaAux[i + 1] = this.pilaAt[i];
+			}
+			this.pilaAt = pilaAux;
+		}
+	}
+	
+	/**
+	 * Añade un elemento en la pila de páginas posteriores.
+	 */
+	private void anadirAd(String f) {
+		if (this.pilaAd.length == 0 || !(f.equals(this.pilaAd[0]))) {
+			String pilaAux[] = new String[this.pilaAd.length + 1];
+			pilaAux[0] = f;
+			for (int i = 0; i < this.pilaAd.length; i++) {
+				pilaAux[i + 1] = this.pilaAd[i];
+			}
+			this.pilaAd = pilaAux;
+		}
+	}
+	
+	/**
+	 * Extrae un elemento de la pila de páginas anteriores.
+	 * 
+	 * @return Página anterior
+	 */
+	private String extraerAt() {
+		if (this.pilaAt.length > 0) {
+			String pos0 = this.pilaAt[0];
+			String pilaAux[] = new String[this.pilaAt.length - 1];
+			for (int i = 0; i < pilaAux.length; i++) {
+				pilaAux[i] = this.pilaAt[i + 1];
+			}
+			this.pilaAt = pilaAux;
+			if (!(pos0.equals(this.fichero))) {
+				return pos0;
+			} else {
+				return this.extraerAt();
+			}
+		} else {
+			return this.fichero;
+		}
+	}
+	
+	/**
+	 * Extrae un elemento de la pila de páginas posteriores.
+	 * 
+	 * @return Página anterior
+	 */
+	private String extraerAd() {
+		if (this.pilaAd.length > 0) {
+			String pos0 = this.pilaAd[0];
+			String pilaAux[] = new String[this.pilaAd.length - 1];
+			for (int i = 0; i < pilaAux.length; i++) {
+				pilaAux[i] = this.pilaAd[i + 1];
+			}
+			this.pilaAd = pilaAux;
+			if (!(pos0.equals(this.fichero))) {
+				return pos0;
+			} else {
+				return this.extraerAd();
+			}
+		} else {
+			return this.fichero;
+		}
+	}
+	
+	/**
+	 * Habilita o deshabilita los botones de adelante y atrás según el estado actual.
+	 */
+	private void colorearBotonesAdelanteAtras() {
+		if (this.pilaAt.length > 0) {
+			this.atras.habilitar();
+		} else {
+			this.atras.deshabilitar();
+		}
+
+		if (this.pilaAd.length > 0) {
+			this.adelante.habilitar();
+		} else {
+			this.adelante.deshabilitar();
+		}
 	}
 }
-
