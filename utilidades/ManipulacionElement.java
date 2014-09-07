16dd85dfaf1555cfdc25b70afdf2a92822bf48f9
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-09-07 18:03:20 +0200

    Formato, Documentación y limpieza de código para la clase ManipulacionElement.

--- utilidades/ManipulacionElement.java
+++ utilidades/ManipulacionElement.java
@@ -1,19 +1,11 @@
-/**
-	Ofrece servicios genéricos sobre manipulación de Element
-	Clase de servicios.
-	
-	@author Antonio Pérez Carrasco
-	@version 2006-2007
-*/
 package utilidades;
 
+import java.io.File;
+import java.io.IOException;
 
-
-
-
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
@@ -22,146 +14,102 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.parsers.*;
 
-import java.io.File;
-import java.io.IOException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+/**
+ * Ofrece servicios genéricos para la manipulación de objetos Element. Clase de
+ * servicios.
+ * 
+ * @author Antonio Pérez Carrasco
+ * @version 2006-2007
+ */
+public class ManipulacionElement {
 
-
-public class ManipulacionElement
-{
 	/**
-		Obtiene un listado de los hijos de un nodo, filtrando los que no son Elements
-		
-		@param e Element del cual se extraerán los hijos Element
-		@return hijos Element de e
-	*/
-	public static Element[] getChildElements(Element e)
-	{
+	 * Obtiene un listado de los hijos de un nodo, filtrando los que no son
+	 * Elements
+	 * 
+	 * @param e
+	 *            Element del cual se extraerán los hijos Element
+	 * @return hijos Element de e
+	 */
+	public static Element[] getChildElements(Element e) {
 		NodeList nl = e.getChildNodes();
 		return nodeListToElementArray(nl);
 	}
 
 	/**
-		Convierte un NodeList en un array de Elements
-		
-		@param nl NodeList que será convertida en un array de Elements
-		@return array de Elements que formaban parte de nl
-	*/
-	public static Element[] nodeListToElementArray(NodeList nl)
-	{
-		int num_elem=0;
-		for (int i=0; i<nl.getLength(); i++)
-			if (nl.item(i) instanceof Element)
-				num_elem++;
-
-		Element el[] = new Element[num_elem];
-		num_elem=0;
-		for (int i=0; i<nl.getLength(); i++)
-			if (nl.item(i) instanceof Element)
-			{
-				el[num_elem]=(Element)(nl.item(i));
+	 * Convierte un NodeList en un array de Elements
+	 * 
+	 * @param nl
+	 *            NodeList que será convertida en un array de Elements
+	 * @return array de Elements que formaban parte de nl
+	 */
+	public static Element[] nodeListToElementArray(NodeList nl) {
+		int num_elem = 0;
+		for (int i = 0; i < nl.getLength(); i++) {
+			if (nl.item(i) instanceof Element) {
 				num_elem++;
 			}
+		}
+
+		Element el[] = new Element[num_elem];
+		num_elem = 0;
+		for (int i = 0; i < nl.getLength(); i++) {
+			if (nl.item(i) instanceof Element) {
+				el[num_elem] = (Element) (nl.item(i));
+				num_elem++;
+			}
+		}
 
 		return el;
 	}
-	
-	/**
-		Muestra la estructura del árbol por terminal
-		
-		@param e árbol DOM que se mostrará
-		@param level valor que determina el margen que se dejará en el terminal
-	*/
-	public static void listChildren(Node e, int level)
-	{
-		//si es un elemento con espacio de nombres asociado...
-		/*if(e instanceof Element && ((Element)e).getLocalName() != null){
-			Element el = (Element)e;
-			print("[ "+el.getPrefix()+" : "+el.getLocalName()+
-				" :: "+el.getNamespaceURI()+" ]", level);
-		} else {
-			println("[ "+e.getNodeName()+" ]", level);
-		}*/
-		
-		/*for (int z=0; z<level; z++)
-			System.out.print("  ");
 
-		if(e instanceof Element)
-			System.out.print("[E ");
-		else
-			System.out.print("[N ");
-		System.out.println(e.getNodeName()+" ]");*/
-
-
-		level++;
-		
-		// imprimimos los atributos, notese que a pesar de que
-		// la variable 'e' debe ser un Elemento para tener atributos,
-		// no necesitamos hacer un cast porque la interfaz Node
-		// provee los metodos para obtener los atributos
-		if(e.hasAttributes())
-		{
-			NamedNodeMap attributes = e.getAttributes();
-			int length = attributes.getLength();
-			Attr attr = null;
-			for(int i=0; i<length; i++){
-				attr = (Attr)attributes.item(i);
-				//print(attr.getNodeName()+"='"+attr.getNodeValue()+"'", level);
-			}
-		}
-			
-		for(Node node = e.getFirstChild(); node != null; node = node.getNextSibling())
-		{
-			listChildren(node, level);
-		}
+	public static void copiarXML(String ficheroEntrada, String ficheroSalida) {
+		Document d = getDocumento(ficheroEntrada);
+		writeXmlFile(d, ficheroSalida);
 	}
 
-	
-	public static void copiarXML(String ficheroEntrada, String ficheroSalida)
-	{
-		Document d=getDocumento(ficheroEntrada);
-		writeXmlFile(d,ficheroSalida);
-	}
-	
-	
 	/**
-		Obtiene el Document de un fichero escrito en formato XML
-		
-		@param fichero dirección del fichero XML
-		@return Document del fichero pasado como parámetro
-	*/
-	public static Document getDocumento(String fichero)
-	{
+	 * Obtiene el Document de un fichero escrito en formato XML
+	 * 
+	 * @param fichero
+	 *            dirección del fichero XML
+	 * @return Document del fichero pasado como parámetro
+	 */
+	public static Document getDocumento(String fichero) {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder =null;
-		Document documento =null;
+		DocumentBuilder builder = null;
+		Document documento = null;
 
-		try{
+		try {
 			builder = factory.newDocumentBuilder();
 			documento = builder.parse(new File(fichero));
-			
-			//listChildren(documento, 0);
-		}catch(ParserConfigurationException e){
-			//System.out.println("ManipulacionElement No se ha podido crear una instancia de DocumentBuilder");
-		}catch(SAXException e){
-			//System.out.println("ManipulacionElement Error SAX al parsear el archivo");
-		}catch(IOException e){
-			//System.out.println("ManipulacionElement Se ha producido un error de entrada salida");
-			//System.out.println(e.getCause());
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return documento;
 	}
-	
-	
-	public static Document getDocument()
-	{
+
+	/**
+	 * Crea y devuelve un nuevo documento.
+	 * 
+	 * @return Nuevo documento.
+	 */
+	public static Document getDocument() {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder =null;
-		Document documento =null;
-		
-		try{
+		DocumentBuilder builder = null;
+		Document documento = null;
+
+		try {
 			builder = factory.newDocumentBuilder();
 			documento = builder.newDocument();
 		} catch (Exception e) {
@@ -169,45 +117,33 @@ public class ManipulacionElement
 		}
 		return documento;
 	}
-	
-	
-	
+
 	/**
-		Escribe un fichero en formato XML, dado un Document
-		
-		@param d Document que se escribirá en fichero
-		@param fichero nombre del fichero que se va a escribir
-	*/
-	public static void writeXmlFile(Document d, String fichero)
-	{
+	 * Escribe un fichero en formato XML, dado un Document
+	 * 
+	 * @param d
+	 *            Document que se escribirá en fichero
+	 * @param fichero
+	 *            nombre del fichero que se va a escribir
+	 */
+	public static void writeXmlFile(Document d, String fichero) {
 		try {
 			// Prepara el Documento DOM para escribirlo
 			Source source = new DOMSource(d);
-    
+
 			// Prepara el fichero de salida
 			File file = new File(fichero);
 			Result result = new StreamResult(file);
 
 			// Escribimos el fichero
-			Transformer xformer = TransformerFactory.newInstance().newTransformer();
+			Transformer xformer = TransformerFactory.newInstance()
+					.newTransformer();
 			xformer.transform(source, result);
 
 		} catch (TransformerConfigurationException e) {
-
+			e.printStackTrace();
 		} catch (TransformerException e) {
-
+			e.printStackTrace();
 		}
-	}
-	
-	/**
-		Imprime una cadena de caracteres dejando un determinado margen a la izquierda
-		
-		@param s cadena de caracteres que se mostrará
-		@param level margen (número de caracteres que se dejarán en blanco en el terminal)
-	*/
-	public static void print(String s, int level){
-		for(; level>0; level--)
-			System.out.print("  ");
-		System.out.println(s);
 	}
 }
