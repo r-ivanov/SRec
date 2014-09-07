58e19b20fae8a4d787ccff6826bf1545c5507d03
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-09-07 18:08:43 +0200

    Formato, Documentación y limpieza de código para la clase NombresYPrefijos.

--- utilidades/NombresYPrefijos.java
+++ utilidades/NombresYPrefijos.java
@@ -1,80 +1,88 @@
-/**
-	Almacena una correspondencia entre un nombre de un método y su prefijo
-	
-*/
 package utilidades;
 
+/**
+ * Almacena una correspondencia entre un nombre de un método y su prefijo. Estos
+ * prefijos son utilizados en la visualización de algoritmos para reducir el
+ * tamaño de los nodos de entrada cuando los nombres de parámetros son muchos
+ * y/o muy largos.
+ */
+public class NombresYPrefijos {
 
+	private String nombre[];
+	private String prefijo[];
 
-
-public class NombresYPrefijos
-{
-	String nombre[];
-	String prefijo[];
-	
-	public NombresYPrefijos()
-	{
-		this.nombre=new String[0];
-		this.prefijo=new String[0];
+	/**
+	 * Crea una nueva instancia de NombresYPrefijos.
+	 */
+	public NombresYPrefijos() {
+		this.nombre = new String[0];
+		this.prefijo = new String[0];
 	}
-	
-	public void add(String nombre, String prefijo)
-	{
-		if (this.nombre.length==0)
-		{
-			this.nombre =new String[1];
-			this.prefijo=new String[1];
-			
-			this.nombre[0]=nombre;
-			this.prefijo[0]=prefijo;
-		}
-		else
-		{
-			String nombreAux[]=new String[this.nombre.length+1];
-			String prefijAux[]=new String[this.prefijo.length+1];
-			
-			for (int i=0; i<this.nombre.length; i++)
-			{
-				nombreAux[i]=this.nombre[i];
-				prefijAux[i]=this.prefijo[i];
+
+	/**
+	 * Añade una correspondencia entre el nombre de un método y un prefijo.
+	 * 
+	 * @param nombre
+	 *            Nombre dle método.
+	 * @param prefijo
+	 *            Prefijo asociado.
+	 */
+	public void add(String nombre, String prefijo) {
+		if (this.nombre.length == 0) {
+			this.nombre = new String[1];
+			this.prefijo = new String[1];
+
+			this.nombre[0] = nombre;
+			this.prefijo[0] = prefijo;
+		} else {
+			String nombreAux[] = new String[this.nombre.length + 1];
+			String prefijAux[] = new String[this.prefijo.length + 1];
+
+			for (int i = 0; i < this.nombre.length; i++) {
+				nombreAux[i] = this.nombre[i];
+				prefijAux[i] = this.prefijo[i];
 			}
-			
-			nombreAux[this.nombre.length]=nombre;
-			prefijAux[this.nombre.length]=prefijo;
-			
-			this.nombre =nombreAux;
-			this.prefijo=prefijAux;
+
+			nombreAux[this.nombre.length] = nombre;
+			prefijAux[this.nombre.length] = prefijo;
+
+			this.nombre = nombreAux;
+			this.prefijo = prefijAux;
 		}
 	}
-	
-	public String[] getCorrespondencia(int i)
-	{
-		String [] c=null;
-		if (i<this.nombre.length)
-		{
-			c=new String[2];
-			c[0]=this.nombre[i];
-			c[1]=this.prefijo[i];
-		}
-		return c;
-	}
-	
-	public String getPrefijo(String nombre)
-	{
-		for (int i=0; i<this.nombre.length; i++)
-			if (this.nombre[i].equals(nombre))
+
+	/**
+	 * Devuelve el prefijo asociado al nombre del método pasado por parámetro.
+	 * 
+	 * @param nombre
+	 *            Nombre del método.
+	 * 
+	 * @return Prefijo asociado, o "No encontrado", si el nombre del método no
+	 *         tiene un prefijo registrado.
+	 */
+	public String getPrefijo(String nombre) {
+		for (int i = 0; i < this.nombre.length; i++) {
+			if (this.nombre[i].equals(nombre)) {
 				return this.prefijo[i];
+			}
+		}
 		return "No encontrado";
 	}
-	
-	public int getLongitudPrefijoMasLargo()
-	{
-		String prefijoLargo="";
-		
-		for (int i=0; i<this.prefijo.length; i++)
-			if (this.prefijo[i].length()>prefijoLargo.length())
-				prefijoLargo=this.prefijo[i];
-		
+
+	/**
+	 * Devuelve la longitud del prefijo más largo registrado.
+	 * 
+	 * @return longitud del prefijo más largo registrado.
+	 */
+	public int getLongitudPrefijoMasLargo() {
+		String prefijoLargo = "";
+
+		for (int i = 0; i < this.prefijo.length; i++) {
+			if (this.prefijo[i].length() > prefijoLargo.length()) {
+				prefijoLargo = this.prefijo[i];
+			}
+		}
+
 		return prefijoLargo.length();
 	}
 }
