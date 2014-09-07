1da2dca94b613ba5dee905f3f223747655350755
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-09-07 16:51:08 +0200

    Formato, Documentación y limpieza de código para la clase Arrays.

--- utilidades/Arrays.java
+++ utilidades/Arrays.java
@@ -1,46 +1,66 @@
 package utilidades;
 
+/**
+ * Clase de utilidades para operaciones con Arrays.
+ */
 public class Arrays {
 
-	
-	public static boolean contiene(int n, int[]array)
-	{
-		for (int i=0; i<array.length; i++)
-			if (array[i]==n)
+	/**
+	 * Permite consultar si un array de enteros contiene un valor concreto.
+	 * 
+	 * @param n
+	 *            valor a consultar.
+	 * @param array
+	 *            Array de entrada.
+	 * 
+	 * @return True si el array contiene el valor, false en caso contrario.
+	 */
+	public static boolean contiene(int n, int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == n) {
 				return true;
+			}
+		}
 		return false;
 	}
-	
-	
-	public static boolean contiene(String n, String[]array)
-	{
-		for (int i=0; i<array.length; i++)
-			if (array[i]!=null && array[i].equals(n))
+
+	/**
+	 * Permite consultar si un array de Strings contiene un valor concreto.
+	 * 
+	 * @param n
+	 *            valor a consultar.
+	 * @param array
+	 *            Array de entrada.
+	 * 
+	 * @return True si el array contiene el valor, false en caso contrario.
+	 */
+	public static boolean contiene(String n, String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null && array[i].equals(n)) {
 				return true;
+			}
+		}
 		return false;
 	}
-	
-	
-	public static int[] insertar(int n, int[]array)
-	{
-		int[]arrayN=new int[array.length+1];
-		
-		for (int i=0; i<array.length; i++)
-			arrayN[i]=array[i];
-		
-		arrayN[arrayN.length-1]=n;
+
+	/**
+	 * Permite añadir un valor al final de un array de enteros.
+	 * 
+	 * @param n
+	 *            valor a introducir.
+	 * @param array
+	 *            Array de entrada.
+	 * 
+	 * @return Array de salida, con el valor añadido.
+	 */
+	public static int[] insertar(int n, int[] array) {
+		int[] arrayN = new int[array.length + 1];
+
+		for (int i = 0; i < array.length; i++) {
+			arrayN[i] = array[i];
+		}
+
+		arrayN[arrayN.length - 1] = n;
 		return arrayN;
 	}
-	
-	public static String[] insertar(String n, String[]array)
-	{
-		String[]arrayN=new String[array.length+1];
-		
-		for (int i=0; i<array.length; i++)
-			arrayN[i]=""+array[i];
-		
-		arrayN[arrayN.length-1]=""+n;
-		return arrayN;
-	}
-	
 }
