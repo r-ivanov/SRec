508f8496ec3ab4a50cd46530903e1c6e598d2025
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-11-30 23:43:47 +0100

    Aristas y reordenación del grafo implementados.

--- org/jgraph/util/NonCollidingEdgeRouter.java
+++ org/jgraph/util/NonCollidingEdgeRouter.java
@@ -1,30 +1,28 @@
 package org.jgraph.util;
 
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.jgraph.JGraph;
 import org.jgraph.graph.AbstractCellView;
-import org.jgraph.graph.AttributeMap.SerializablePoint2D;
 import org.jgraph.graph.CellView;
 import org.jgraph.graph.DefaultGraphModel;
-import org.jgraph.graph.Edge;
 import org.jgraph.graph.EdgeView;
 import org.jgraph.graph.GraphConstants;
 import org.jgraph.graph.GraphLayoutCache;
 import org.jgraph.graph.GraphModel;
 import org.jgraph.graph.DefaultEdge.LoopRouting;
-import org.jgraph.graph.PortView;
-import org.w3c.dom.views.AbstractView;
 
 /**
  * Implementación que permite evitar que las aristas pasen por encima de los
  * nodos.
  */
 public class NonCollidingEdgeRouter extends LoopRouting {
+	
+	private static final long serialVersionUID = 7596676104954178713L;
 	
 	private JGraph graph;
 	
@@ -46,117 +44,152 @@ public class NonCollidingEdgeRouter extends LoopRouting {
 			nodeTo = edge.getTargetParentView();
 		}
 		if ((null == nodeFrom) || (null == nodeTo)) {
-			// System.out.println("EdgeView has no source or target view : "
-			// + edge.toString());
 			return null;
 		}
 		if (nodeFrom == nodeTo) {
-			// System.out.println("nodeFrom and NodeTo are the same cell view");
 			return null;
 		}
 
-//		List points = edge.getPoints();
-//		Object startPort = points.get(0);
-//		Object endPort = points.get(points.size() - 1);
-
 		Point2D nodeFromLocation = AbstractCellView.getCenterPoint(nodeFrom);
 		Point2D nodeToLocation = AbstractCellView.getCenterPoint(nodeTo);
-
-		System.out.println("startPortPoint: " + nodeFromLocation);
-		System.out.println("endPortPoint: " + nodeToLocation);
-		boolean intersects = obtenerCeldasEntrePuntos(cache, edge, nodeFromLocation, nodeToLocation);
-		System.out.println("==================================");
-		System.out.println("");
-
+		
 		newPoints.add(nodeFrom);
-		if (intersects) {
-			newPoints.add(new Point2D.Double(nodeFromLocation.getX(), nodeFromLocation.getY() - 50));
-		}
+		newPoints.addAll(obtenerPuntosArista(cache, edge, nodeFromLocation, nodeToLocation));
 		newPoints.add(nodeTo);
+		
 		return newPoints;
 	}
 
-	private boolean obtenerCeldasEntrePuntos(GraphLayoutCache cache,
-			EdgeView edgeView, Point2D from, Point2D to) {
-
-		List<CellView> celdasEntrePuntos = new ArrayList<CellView>();
-		
-		boolean intersects = false;
+	private List<Point2D> obtenerPuntosArista(GraphLayoutCache cache, EdgeView edgeView, Point2D from, Point2D to) {
 		
 		GraphModel model = cache.getModel();
-		CellView[] celdas = cache.getAllViews();
-
+		CellView[] celdas = cache.getAllViews();		
+		List<CellView> nodos = new ArrayList<CellView>();
 		for (int i = 0; i < celdas.length; i++) {
 			Object modeloCelda = celdas[i].getCell();
 			if (DefaultGraphModel.isVertex(model, modeloCelda) && !celdas[i].getCell().equals(edgeView.getSource().getParentView().getCell())
 					&& !celdas[i].getCell().equals(edgeView.getTarget().getParentView().getCell())) {				
-				intersects =  celdas[i].getBounds().intersectsLine(from.getX(), from.getY(), to.getX(), to.getY());
-				System.out.println("Intersecciona?: " + intersects);
-				if (intersects) {
-					System.out.println("Intersecciona!!!!!");
-				}
-				
+				nodos.add(celdas[i]);
 			}
 		}
-
-		return intersects;
+		
+		int totalNodos = nodos.size();
+		int margenArista = 10;
+		List<Point2D> verticesParaCurva = new ArrayList<Point2D>();		
+		Line2D lineaArista = new Line2D.Double(from, to);
+		
+		Rectangle2D rectanguloActual = null;
+		Rectangle2D rectanguloObtenido = null;
+		do {	
+			List<Point2D> puntosArista = new ArrayList<Point2D>();
+			puntosArista.add(from);
+			puntosArista.addAll(verticesParaCurva);
+			puntosArista.add(to);
+			
+			rectanguloActual = rectanguloObtenido;
+			rectanguloObtenido = obtenerRectanguloARodear(nodos, rectanguloActual, puntosArista, margenArista);
+			if (rectanguloObtenido != null && !rectanguloObtenido.equals(rectanguloActual)) {
+				margenArista = (totalNodos - nodos.size()) * 10;
+				verticesParaCurva = obtenerVerticesParaCurva(lineaArista, rectanguloObtenido, margenArista, edgeView);
+			}
+		
+		} while (rectanguloObtenido != null && !rectanguloObtenido.equals(rectanguloActual));
+		
+		return verticesParaCurva;
 	}
-
-//	/**
-//	 * Determina el vertice de la celda por el que la arista debe bordear para
-//	 * evitar la colision con la celda.
-//	 * 
-//	 * @param cellView
-//	 *            Celda
-//	 * @param edgeStart
-//	 *            Punto de comienzo de la arista
-//	 * @param edgeEnd
-//	 *            Punto final de la arista
-//	 * 
-//	 * @return Punto para evitar colision, null si no hay colision.
-//	 */
-//	private boolean interseccionAristaConCelda(CellView cellView,
-//			Point2D edgeStart, Point2D edgeEnd) {
-//		
-//		if (edgeStart.getY() == edgeEnd.getY()) {
-//			if (cellView.edgeStart.getY())
-//		}
-//		
-//		double interseccionSuperior;
-//		double intersectionInferior;
-//		double puntoSuperior;
-//		double puntoInferior;
-//
-//		// Calcular m y c para la ecuacuón de la recta (y = mx+c)
-//		double m = (edgeEnd.getY() - edgeStart.getY()) / (edgeEnd.getX() - edgeStart.getX());
-//		double c = edgeStart.getY() - (m * edgeStart.getX());
-//
-//		if (m > 0) {
-//			interseccionSuperior = (m * cellView.getBounds().getX() + c);
-//			intersectionInferior = (m * (cellView.getBounds().getX() + cellView.getBounds().getWidth()) + c);
-//		} else {
-//			interseccionSuperior = (m * (cellView.getBounds().getX() + cellView.getBounds().getWidth()) + c);
-//			intersectionInferior = (m * cellView.getBounds().getX() + c);
-//		}
-//
-//		if (edgeStart.getY() < edgeEnd.getY()) {
-//			puntoSuperior = edgeStart.getY();
-//			puntoInferior = edgeEnd.getY();
-//		} else {
-//			puntoSuperior = edgeEnd.getY();
-//			puntoInferior = edgeStart.getY();
-//		}
-//
-//		double topOverlap;
-//		double botOverlap;
-//		
-//		topOverlap = interseccionSuperior > puntoSuperior ? interseccionSuperior : puntoSuperior;
-//		botOverlap = intersectionInferior < puntoInferior ? intersectionInferior
-//				: puntoInferior;
-//
-//		return (topOverlap < botOverlap)
-//				&& (!((botOverlap < cellView.getBounds().getY()) || (topOverlap > cellView.getBounds().getY() + cellView.getBounds().getHeight())));
-//
-//	}
-
+	
+	private Rectangle2D obtenerRectanguloARodear(List<CellView> nodos, Rectangle2D rectangulo, List<Point2D> puntosArista, double margenArista) {
+		
+		List<CellView> copiaNodos = new ArrayList<CellView>(nodos);
+		for (int i = 0; i < puntosArista.size() - 1; i++) {
+			Line2D lineaArista = new Line2D.Double(puntosArista.get(i), puntosArista.get(i + 1));
+			for (CellView nodo : copiaNodos) {
+				Rectangle2D boundsNodo = nodo.getBounds();
+				if (lineaArista.intersects(boundsNodo.getX() - margenArista,
+						boundsNodo.getY() - margenArista,
+						boundsNodo.getWidth() + margenArista,
+						boundsNodo.getHeight() + margenArista)) {
+					
+					if (rectangulo == null) {
+						rectangulo = new Rectangle2D.Double(nodo.getBounds().getX(), nodo.getBounds().getY(),
+								nodo.getBounds().getWidth(), nodo.getBounds().getHeight());
+					} else {
+						rectangulo = rectangulo.createUnion(nodo.getBounds());
+					}
+					nodos.remove(nodo);
+				}
+			}
+		}
+		
+		return rectangulo;
+	}
+	
+	private List<Point2D> obtenerVerticesParaCurva(Line2D lineaArista, Rectangle2D rectangulo, double margenArista, EdgeView edge) {
+		
+		List<Point2D> verticesSuperiores = new ArrayList<Point2D>();
+		List<Point2D> verticesInferiores = new ArrayList<Point2D>();
+		
+		List<Point2D> verticesRectangulo = new ArrayList<Point2D>();
+		verticesRectangulo.add(new Point2D.Double(rectangulo.getMinX() - margenArista, rectangulo.getMinY() - margenArista));
+		verticesRectangulo.add(new Point2D.Double(rectangulo.getMaxX() + margenArista, rectangulo.getMinY() - margenArista));
+		verticesRectangulo.add(new Point2D.Double(rectangulo.getMinX() - margenArista, rectangulo.getMaxY() + margenArista));
+		verticesRectangulo.add(new Point2D.Double(rectangulo.getMaxX() + margenArista, rectangulo.getMaxY() + margenArista));
+		
+		for(Point2D vertice : verticesRectangulo) {
+			if (lineaArista.relativeCCW(vertice) > 0) {
+				verticesSuperiores.add(vertice);
+			} else {
+				verticesInferiores.add(vertice);
+			}
+		}
+		
+		double distanciaTotalVerticesSuperiores = 0;
+		double distantiaTotalVerticesInferiores = 0;	
+		for(Point2D vertice : verticesSuperiores) {
+			distanciaTotalVerticesSuperiores += lineaArista.ptLineDist(vertice);
+		}
+		for(Point2D vertice : verticesInferiores) {
+			distantiaTotalVerticesInferiores += lineaArista.ptLineDist(vertice);
+		}
+		
+		List<Point2D> verticesADevolver;
+		if (distanciaTotalVerticesSuperiores < distantiaTotalVerticesInferiores) {
+			verticesADevolver = verticesSuperiores;
+		} else {
+			verticesADevolver = verticesInferiores;
+		}
+		
+		if (verticesADevolver.size() == 2) {
+			Point2D vertice1 = verticesADevolver.get(0);
+			Point2D vertice2 = verticesADevolver.get(1);
+			
+			Point2D verticeMasLejano;
+			Point2D verticeMasCercano;
+			if (lineaArista.ptLineDist(vertice1) > lineaArista.ptLineDist(vertice2)) {
+				verticeMasLejano = vertice1;
+				verticeMasCercano = vertice2;
+			} else {
+				verticeMasLejano = vertice2;
+				verticeMasCercano = vertice1;
+			}
+			
+			Line2D lineaVerticeMasLejanoANodoFinal = new Line2D.Double(verticeMasLejano, lineaArista.getP2());
+			Line2D lineaNodoInicialAVerticeMasLejano = new Line2D.Double(lineaArista.getP1(), verticeMasLejano);
+			
+			if (!lineaVerticeMasLejanoANodoFinal.intersects(rectangulo) && !lineaNodoInicialAVerticeMasLejano.intersects(rectangulo)) {
+				verticesADevolver.remove(verticeMasCercano);
+			} else {
+				verticesADevolver.clear();
+				if (lineaArista.getP1().distance(vertice1) > lineaArista.getP1().distance(vertice2)) {
+					verticesADevolver.add(vertice2);
+					verticesADevolver.add(vertice1);
+				} else {
+					verticesADevolver.add(vertice1);
+					verticesADevolver.add(vertice2);
+				}
+			}			
+		}
+		
+		return verticesADevolver;
+	}
 }
