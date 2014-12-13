7e66f2fb30d40db6ac813c568023cc12d5081dae
David Pastor Herranz <d.pastor.herranz@gmail.com>
2014-12-13 19:09:39 +0100

    Pintando los nodos del grafo de dependencia por pantalla.

--- datos/NodoGrafoDependencia.java
+++ datos/NodoGrafoDependencia.java
@@ -1,17 +1,62 @@
 package datos;
 
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class NodoGrafoDependencia {
+import javax.swing.BorderFactory;
 
+import org.jgraph.graph.DefaultEdge;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.DefaultPort;
+import org.jgraph.graph.Edge;
+import org.jgraph.graph.GraphCell;
+import org.jgraph.graph.GraphConstants;
+import org.jgraph.util.NonCollidingEdgeRouter;
+
+import conf.Conf;
+
+public class NodoGrafoDependencia {
+	
+	private static final Edge.Routing edgeRouter;
+	static {
+		edgeRouter = new NonCollidingEdgeRouter();
+	}
+	
 	private List<NodoGrafoDependencia> dependencias;
 	private RegistroActivacion registroActivacion;
 	
+	private DefaultGraphCell celdaGrafo;
+	private DefaultPort portCelda;
+	private List<DefaultEdge> aristas;
+	
 	public NodoGrafoDependencia(RegistroActivacion registroActivacionAsociado) {
 		this.dependencias = new ArrayList<NodoGrafoDependencia>();
+		this.aristas = new ArrayList<DefaultEdge>();
 		this.registroActivacion = registroActivacionAsociado;
+		
+		String repEntrada = registroActivacionAsociado.getEntrada().getRepresentacion();
+		if (repEntrada.length() < 3) {
+			repEntrada = "  " + repEntrada + "  ";
+		}
+
+		this.celdaGrafo = new DefaultGraphCell(repEntrada);
+		GraphConstants.setFont(this.celdaGrafo.getAttributes(), new Font("Arial", Font.BOLD, 20));
+		GraphConstants.setDisconnectable(this.celdaGrafo.getAttributes(), false);
+		GraphConstants.setMoveable(this.celdaGrafo.getAttributes(), true);
+		GraphConstants.setSelectable(this.celdaGrafo.getAttributes(), true);
+		GraphConstants.setResize(this.celdaGrafo.getAttributes(), false);
+		GraphConstants.setOpaque(this.celdaGrafo.getAttributes(), true);
+		GraphConstants.setForeground(this.celdaGrafo.getAttributes(), Conf.colorFEntrada);
+		GraphConstants.setBackground(this.celdaGrafo.getAttributes(), (Conf.colorC1AEntrada));
+		GraphConstants.setGradientColor(this.celdaGrafo.getAttributes(), (Conf.colorC2AEntrada));
+		//GraphConstants.setBounds(celda1.getAttributes(), new Rectangle(200, 200, 75, 40));
+		GraphConstants.setAutoSize(this.celdaGrafo.getAttributes(), true);
+		GraphConstants.setBorder(this.celdaGrafo.getAttributes(), BorderFactory.createBevelBorder(0));
+		GraphConstants.setSizeable(this.celdaGrafo.getAttributes(), false);
+		this.portCelda = new DefaultPort();
+		this.celdaGrafo.add(this.portCelda);
 	}
 
 	public boolean equals(NodoGrafoDependencia nodo) {
@@ -19,6 +64,10 @@ public class NodoGrafoDependencia {
 				this.nombreParametrosEntradaIguales(nodo) &&
 				this.valorParametrosEntradaIguales(nodo) &&
 				this.nombreMetodoIgual(nodo);
+	}
+	
+	public String[] getParams() {
+		return this.registroActivacion.getParametros();
 	}
 	
 	private boolean nombreMetodoIgual(NodoGrafoDependencia nodo) {
@@ -50,8 +99,27 @@ public class NodoGrafoDependencia {
 		return iguales;
 	}
 	
-	public void addDependencia(NodoGrafoDependencia nodo) {
+	public void addDependencia(NodoGrafoDependencia nodo) {		
+		DefaultEdge arista = new DefaultEdge();
+		GraphConstants.setLineEnd(arista.getAttributes(), GraphConstants.ARROW_CLASSIC);
+		GraphConstants.setEndFill(arista.getAttributes(), true);
+		GraphConstants.setSelectable(arista.getAttributes(),false);
+		GraphConstants.setLineWidth(arista.getAttributes(), 1);
+		GraphConstants.setLineColor(arista.getAttributes(), Conf.colorFlecha);
+		GraphConstants.setRouting(arista.getAttributes(), edgeRouter);
+		GraphConstants.setLineStyle(arista.getAttributes(), GraphConstants.STYLE_SPLINE);
+		arista.setSource(this.portCelda);
+		arista.setTarget(nodo.portCelda);
+		
+		this.aristas.add(arista);
 		this.dependencias.add(nodo);
+	}
+	
+	public List<GraphCell> obtenerCeldasDelNodoParaGrafo() {
+		List<GraphCell> celdas = new ArrayList<GraphCell>();
+		celdas.add(this.celdaGrafo);
+		celdas.addAll(aristas);
+		return celdas;
 	}
 	
 	public List<NodoGrafoDependencia> getDependencias() {
